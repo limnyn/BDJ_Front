f2d5d7a20f49b0229d77928ab613373da609f63f
Limnyn <hogandi001@gmail.com>
2023-09-18 19:25:15 +0900

    add youtube widget in detail_summary

--- lib/detail_summary.dart
+++ lib/detail_summary.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 class DetailSummary extends StatelessWidget {
   final String video_id;
   final String title;
@@ -13,6 +14,16 @@ class DetailSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _controller = YoutubePlayerController.fromVideoId(
+      videoId: video_id,
+      autoPlay: false,
+      params: const YoutubePlayerParams(
+          mute: false,
+          showControls: true,
+          showFullscreenButton: true
+      ),
+    );
+
     String maxWidthString = dotenv.get('MAX_WIDTH');
     double maxWidth = 700; // 기본값 설정
     if (maxWidthString.isNotEmpty) {
@@ -22,62 +33,62 @@ class DetailSummary extends StatelessWidget {
       }
     }
     double windowWidth = MediaQuery.of(context).size.width;
-    double widgetWidth = (windowWidth > maxWidth ? maxWidth: windowWidth);
+    double widgetWidth = (windowWidth > maxWidth ? maxWidth : windowWidth);
+
     return Scaffold(
-      // appBar: PreferredSize(
-      //   preferredSize: Size.fromHeight(35.0),
-      //   child: AppBar(title: Text('Detail',style: TextStyle(color: Colors.grey[500]),),centerTitle: true,elevation: 0.0,backgroundColor: Colors.grey[800],)
-      // ),
       backgroundColor: Colors.grey[900],
-
-      body:
-          SafeArea(
-            child: Align(
-                alignment: Alignment.center,
-                child:
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: widgetWidth,
+            child: Column(
+              children: [
                 Container(
-                  width: widgetWidth,
-                  child: Column(
-                    children: [
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        child: OutlinedButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-
-                          child: Text('<', style: TextStyle(color: Colors.grey),),
-                        ),
-                      ),
-
-                      Text(video_id, style: TextStyle(fontSize: 10, color: Colors.grey[500])),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
-                        width: double.infinity,
-                        height: 80,
-                        color: Colors.grey[830],
-                        child: Text(
-                          title,
-                          style: TextStyle(fontSize: 16, color: Colors.grey[500]),
-                        ),
-                      ),
-                      Expanded(
-                        child: SingleChildScrollView(
-                          padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-                          child: Container(
-                            width: double.infinity,
-                            color: Colors.grey[830],
-                            child: Text(
-                              summary,
-                              style: TextStyle(fontSize: 13, color: Colors.grey[500]),
-                            ),
-                          ),
-                        ),
-                      ),],
+                  alignment: Alignment.centerLeft,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('<', style: TextStyle(color: Colors.grey)),
                   ),
-                )
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child:YoutubePlayer(
+                    controller: _controller,
+                  )
+
+                ),
+                Text(video_id, style: TextStyle(fontSize: 10, color: Colors.grey[500])),
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  width: double.infinity,
+                  height: 80,
+                  color: Colors.grey[830],
+                  child: Text(
+                    title,
+                    style: TextStyle(fontSize: 16, color: Colors.grey[500]),
+                  ),
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                    child: Container(
+                      width: double.infinity,
+                      color: Colors.grey[830],
+                      child: Text(
+                        summary,
+                        style: TextStyle(fontSize: 13, color: Colors.grey[500]),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+        ),
+      ),
     );
   }
 }
